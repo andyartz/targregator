c65ded643e27d9f5130027213c6503f818e96282
Andy Artz <andyartz@andyartz.com>
2020-10-03 14:06:33 -0500

    Initial Commit.

---
+++ src/main/groovy/com/andyartz/targregator/TargregatorApplication.groovy
@@ -0,0 +1,12 @@
+package com.andyartz.targregator
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TargregatorApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TargregatorApplication, args)
+    }
+}
